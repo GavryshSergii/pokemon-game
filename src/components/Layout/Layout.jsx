b8a4a3a9267ecf144c44eea60423809b1fab2148
@s.havrysh.2107 <serge.gavrysh@gmail.com>
2021-09-10 17:19:12 +0300

    homework-3

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,36 +1,26 @@
 import style from './style.module.css';
-import classNames from 'classnames';
 
-export default function Layout({title, descr, urlBg, colorBg}) {
+export default function Layout({title, urlBg, colorBg, children}) {
 
-    let sectionStyle= {};
+    let sectionStyle = {};
     if (urlBg) {
         sectionStyle = {background: `url(${urlBg})`}
     } else if (colorBg) {
         sectionStyle = {background: colorBg}
     }
 
-    const descClasses = classNames(style.desc, style.full);
     return (
         <>
             <section className={style.root} style={sectionStyle}>
                 <div className={style.wrapper}>
                     <article>
-                        {
-                            title && (
-                                <div className={style.title}>
-                                    <h3>{title}</h3>
-                                    <span className={style.separator}></span>
-                                </div>
-                            )
-                        }
-                        {
-                            descr && (
-                                <div className={descClasses}>
-                                    <p>{descr}</p>
-                                </div>
-                            )
-                        }
+                        <div className={style.title}>
+                            <h3>{title}</h3>
+                            <span className={style.separator}/>
+                        </div>
+                        <div className={`${style.desc} ${style.full}`}>
+                            {children}
+                        </div>
                     </article>
                 </div>
             </section>
